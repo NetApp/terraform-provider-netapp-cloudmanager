b1bfc01916dee9ec90e02a5b45f4e61f66071a0a
Warren Barnes <warrenb@netapp.com>
2023-05-04 09:15:31 +0100

    Update config.go

--- cloudmanager/config.go
+++ cloudmanager/config.go
@@ -3,6 +3,7 @@ package cloudmanager
 import (
 	"fmt"
 	"log"
+	"strings"
 )
 
 // Config is a struct for user input
@@ -16,6 +17,7 @@ type configStruct struct {
 	AWSProfile         string
 	AWSProfileFilePath string
 	AzureAuthMethods   []string
+	ConnectorHost      string
 }
 
 // Client is the main function to connect to the APi
@@ -24,9 +26,9 @@ func (c *configStruct) clientFun() (*Client, error) {
 	if c.Environment == "prod" {
 		log.Print("Prod Environment")
 		client = &Client{
-			CloudManagerHost:     "https://api.bluexp.netapp.com",
-			AuthHost:             "https://api.bluexp.netapp.com/auth/oauth/token",
-			SaAuthHost:           "https://api.bluexp.netapp.com/auth/oauth/token",
+			CloudManagerHost:     "https://cloudmanager.cloud.netapp.com",
+			AuthHost:             "https://netapp-cloud-account.auth0.com/oauth/token",
+			SaAuthHost:           "https://cloudmanager.cloud.netapp.com/auth/oauth/token",
 			Audience:             "https://api.cloud.netapp.com",
 			Auth0Client:          "Mu0V1ywgYteI6w1MbD15fKfVIUrNXGWC",
 			AMIFilter:            "Setup-As-Service-AMI-Prod*",
@@ -35,14 +37,14 @@ func (c *configStruct) clientFun() (*Client, error) {
 			GCPCompute:           "https://compute.googleapis.com",
 			GCPImageProject:      "netapp-cloudmanager",
 			GCPImageFamily:       "cloudmanager",
-			CVSHostName:          "https://api.bluexp.netapp.com/cloud-volumes/cvs",
+			CVSHostName:          "https://api.services.cloud.netapp.com",
 		}
 	} else if c.Environment == "stage" {
 		log.Print("Stage Environment")
 		client = &Client{
-			CloudManagerHost:        "https://staging.api.bluexp.netapp.com",
-			AuthHost:                "https://staging.api.bluexp.netapp.com/auth/oauth/token",
-			SaAuthHost:              "https://staging.api.bluexp.netapp.com/auth/oauth/token",
+			CloudManagerHost:        "https://staging.cloudmanager.cloud.netapp.com",
+			AuthHost:                "https://staging-netapp-cloud-account.auth0.com/oauth/token",
+			SaAuthHost:              "https://staging.cloudmanager.cloud.netapp.com/auth/oauth/token",
 			Audience:                "https://api.cloud.netapp.com",
 			Auth0Client:             "O6AHa7kedZfzHaxN80dnrIcuPBGEUvEv",
 			AMIFilter:               "Setup-As-Service-AMI-*",
@@ -52,10 +54,10 @@ func (c *configStruct) clientFun() (*Client, error) {
 			GCPImageProject:         "tlv-automation",
 			GCPImageFamily:          "occm-automation",
 			AzureEnvironmentForOCCM: "stage",
-			CVSHostName:             "https://staging.api.bluexp.netapp.com/cloud-volumes/cvs",
+			CVSHostName:             "https://staging.api.services.cloud.netapp.com",
 		}
 	} else {
-		return &Client{}, fmt.Errorf("expected environment to be one of [prod stage]: %s", c.Environment)
+		return &Client{}, fmt.Errorf("expected environment to be one of [prod stage], %s", c.Environment)
 	}
 
 	if c.SaSecretKey != "" && c.SaClientID != "" {
@@ -64,6 +66,16 @@ func (c *configStruct) clientFun() (*Client, error) {
 		client.SetRefreshToken(c.RefreshToken)
 	} else {
 		return &Client{}, fmt.Errorf("expected refresh_token or sa_secret_key and sa_client_id")
+	}
+
+	if c.ConnectorHost != "" {
+		if strings.HasPrefix(c.ConnectorHost, "https://") {
+			client.CloudManagerHost = c.ConnectorHost
+		} else if strings.HasPrefix(c.ConnectorHost, "http://") {
+			client.CloudManagerHost = c.ConnectorHost
+		} else {
+			client.CloudManagerHost = fmt.Sprintf("https://%s", c.ConnectorHost)
+		}
 	}
 
 	if c.Simulator {
